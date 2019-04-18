31799ab0bae1f700cfbe3e4df782b46aba2f76e8
Dani <danicpu1802@gmail.com>
2019-04-18 13:06:06 +0200

    Fibonacci sequence writer
    Writes the first n numbers from the Fibonacci sequence while having n inputed by the user
    
    Signed-off-by: Dani <danicpu1802@gmail.com>

--- practica.sh
+++ practica.sh
@@ -6,11 +6,27 @@
 # xargs => takes the input from the previous command and passes it as argument to ping
 # | (pipe) => connects two commands through standard file interface
 
-awk '{print $1}' input.txt | xargs -r -I '{}' \
-  bash -c "echo '{}' | grep -q -e ':' && \
-    ping6 -c 1 '{}' -W 1 || \
-    ping -c 1 '{}' -W 1"
+# awk '{print $1}' input.txt | xargs -r -I '{}' \
+#  bash -c "echo '{}' | grep -q -e ':' && \
+#    ping6 -c 1 '{}' -W 1 || \
+#   ping -c 1 '{}' -W 1"
 
 #TODO Dan: Print the first x numbers from Fibonacci in a file
-
+a=1
+b=1
+echo '~~The following programme will write the first x numbers from the Fibonacci sequence in a file~~'
+echo '~~Please enter an input value for x~~'
+read x
+if [ -f outputdan.txt ] #tests if the file exists
+then
+rm outputdan.txt           ##deletes the file if it exists in order to avoid appending to the file already containing values
+fi
+echo '~~The first '$x' number from the Fibonacci sequence are~~'
+for (( c=1; c<=x; c++ )) 
+do
+b=$a                    
+a=$((a+b))
+echo $a >>outputdan.txt     #appends the result into the file
+done
+cat outputdan.txt
 #TODO Irina: Search for the word "Elisabeth" in a file; insert "the first" after it if it doesn't exist
